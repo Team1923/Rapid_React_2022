6d4f4804cab353690b2ae7827e2e78d214ae4781
Aalind Tyagi <tyagiaalind@gmail.com>
2022-02-25 20:53:05 -0500

    fixed 2 ball auton

--- src/main/java/frc/robot/commands/Autons/TwoBallHighAuto.java
+++ src/main/java/frc/robot/commands/Autons/TwoBallHighAuto.java
@@ -1,10 +1,11 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+ 
 package frc.robot.commands.Autons;
-
+ 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.ConveyorCommands.AutoConveyor;
 import frc.robot.commands.DriveTrainCommands.AutoDrive;
@@ -15,7 +16,7 @@ import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.DualRollerLauncher;
 import frc.robot.subsystems.IntakeSubsystem;
-
+ 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
@@ -36,14 +37,25 @@ public class TwoBallHighAuto extends SequentialCommandGroup {
                 new AutoIntake(intake, 0.9),
                 new SequentialCommandGroup(
                     new AutoDrive(drive, 0.75, 0.15).withTimeout(0.7), // fix time
-                    new SpinUpLowOnce(drl, 2600, 900),
+                    new SpinUpLowOnce(drl, 2700, 900),
                     new ParallelCommandGroup(
-                            new MaintainVelocity(drl, 2600, 900),
-                            new AutoDrive(drive, -0.75, 0.275))
-                        .withTimeout(2.25),
+                            new MaintainVelocity(drl, 2700, 900),
+                            new AutoDrive(drive, -0.675, 0.275))
+                        .withTimeout(2.5),
                     new ParallelCommandGroup(
-                        new MaintainVelocity(drl, 2600, 900).withTimeout(10),
-                        new AutoDrive(drive, -0.5, 0).withTimeout(0.5),
-                        new AutoConveyor(conveyor, -0.9, -0.9).withTimeout(5))))));
+                        new MaintainVelocity(drl, 2700, 900).withTimeout(10),
+                        new SequentialCommandGroup(
+                            new AutoDrive(drive, -0.5, 0).withTimeout(0.9),
+                            new AutoConveyor(conveyor, -0.9, -0.9).withTimeout(.3),
+                            new RunCommand(() -> {}).withTimeout(0.5),
+                            new AutoConveyor(conveyor, -0.9, -0.9).withTimeout(.3),
+                            new RunCommand(() -> {}).withTimeout(0.5),
+                            new AutoConveyor(conveyor, -0.9, -0.9).withTimeout(.3),
+                            new RunCommand(() -> {}).withTimeout(0.5),
+                            new AutoConveyor(conveyor, -0.9, -0.9).withTimeout(.3),
+                            new RunCommand(() -> {}).withTimeout(0.5)
+                            ))))));
   }
 }
+ 
+
